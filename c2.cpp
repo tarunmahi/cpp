84968ebcf4cabf9e1f78991bf83367d335dcacb5
tarunmahi <tarunmahendra007@gmail.com>
2022-08-01 00:14:33 +0530

    added switch statements in ./basics/c3

--- c2.cpp
+++ c2.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main(){
-    int a= 80;
     int b=0120;
     int c = 0x50;
     int d= 0b01010000;
-    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d;
+    cout<<"\n"<<b<<"\n"<<c<<"\n"<<d;
+     
+    char a =66;
+    cout<<"value is "<<a<<"\n";
+    cout<<"values is "<<static_cast<int>(a)<<"\n";
+
 }
---
+++ c3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<limits>
+
+  const int pen{10};
+  const int pencil{29};
+  const int brush{1};
+  const int scope{25};
+  const int valient{59};
+  const int scissor{80};
+int main(){
+ //   std::cout<<"the max and min value of int is "<<std::numeric_limits<int>::min() <<" to "<<std::numeric_limits<int>::max()<<" ";
+ //std::cout<<"the min and max vaues of "<<std::numeric_limits<double>::min()<<"to"<<std::numeric_limits<double>::max()<<" ";   
+
+   int op{scissor};
+   switch(op){
+    case pen : {
+        std::cout<<"the tool is pen";
+    }
+    break;
+    case scissor :{
+        std::cout<<"tool is scissor";
+    }
+    break;
+    default :{
+        std::cout<<"move on";
+    }
+   }
+
+
+
+}
+
+
+
