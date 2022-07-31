dc3b5184b5ad5448b75de9bd372a967ca76883d2
tarunmahi <tarunmahendra007@gmail.com>
2022-08-01 00:15:58 +0530

    added switch statements in ./basics/c3

--- c3.cpp
+++ c3.cpp
@@ -11,7 +11,7 @@ int main(){
  //   std::cout<<"the max and min value of int is "<<std::numeric_limits<int>::min() <<" to "<<std::numeric_limits<int>::max()<<" ";
  //std::cout<<"the min and max vaues of "<<std::numeric_limits<double>::min()<<"to"<<std::numeric_limits<double>::max()<<" ";   
 
-   int op{scissor};
+   int op{scissor};;
    switch(op){
     case pen : {
         std::cout<<"the tool is pen";
